508690c7155dc358dfff0666751535b5c0f7dcd8
danamyers <vicentejuarezdanarubi@cetis50cdmx.com>
2025-09-24 19:20:34 -0600

    Add files via upload

---
+++ librosatr.py
@@ -0,0 +1,43 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title="Galeria"
+    page.bgcolorft.Colors.BLACK45
+    
+    libros = [
+        {
+            "titulo": "Noche estrellada",
+            "autor": "Vincent van Gogh",
+            "año": 1889,
+            "descripcion": "pintura postimpresionista de un cielo nocturno arremolinado y expresivo, con Venus",
+            "imagen": "https://share.google/images/GOFdyHrmfp47OrOQF"
+        },
+        {
+            "titulo": "El sueño",
+            "autor": "Pablo picasso",
+            "año": 1932,
+            "descripcion": "La obra de Picasso (1932) muestra a su amante Marie-Thérèse Walter en un estado de ensueño",
+            "imagen": "https://share.google/images/V0Q4LAV2udiCnJ4V1",
+        },
+        {
+            "titulo": "Lirios",
+            "autor": "Vicent Van Gogh",
+            "año": 1889,
+            "descripcon": "Los lirios es una célebre pintura de Vincent van Gogh de 1889, que muestra un campo de lirios en el jardín de un asilo",
+            "imagene": "https://share.google/images/gn5RrugImPEiYVpfr",
+        },
+        {
+           "titulo": "culpa mia",
+           "autor": "Mercedes Ron López",
+           "año": 2020,
+           "descripcion": "Nick lester ha sido creado para amargarme la viva Alto, ojos azules, pelo negro como la noche",
+           "imagen": "https://share.google/images/CHOX8a44xYjWkPR8h",
+        },
+        {
+            "titulo": "culpa tuya",
+            "autor": "Mercedes Ron López",
+            "año": 2024,
+            "descripcion": "es una película y un libro de romance juvenil que continúa la historia de Noah y Nick, una relación prohibida entre hermanastros",
+            "imagen": "https://share.google/images/f5dWI4MkHAg8BF45c",
+        },
